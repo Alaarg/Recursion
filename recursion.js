aa4663d61c47b84b3fb99cbf7f3bfd0005271563
alaarg <ahmadalaarg123@gmail.com>
2019-07-10 17:29:00 +0300

    recursion 5:28

--- recursion.js
+++ recursion.js
@@ -8,9 +8,28 @@ Ex: sum(2); => 2 + 1 + 0 => 3
 Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
 
 */
+
 function sum(n) {
-  // your code here 
+  if (n === 0) {
+
+    return 0;
+  }
+  return (n + sum(n - 1));
 }
+// console.log(sum(10))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 2
@@ -23,8 +42,12 @@ Ex: factorial(4); => 4 * 3 * 2 * 1 => 24
 
 */
 function factorial(n) {
-  // your code here 
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
+// console.log(factorial(4))
 
 
 /*
@@ -37,7 +60,16 @@ Ex: repeatStr("to",2); => "to to"
 Ex: repeatStr("to",4); => "to to to to"
 */
 
+function repeatStr(x, z) {
+  if (z === 0) {
+    return "";
 
+  }
+
+
+  return x + repeatStr(x, z - 1);
+}
+// console.log(repeatStr("alaarg ", 3))
 /*
 4
 Create a function called fibon
@@ -49,113 +81,214 @@ Ex: fibon(2); => 2
 Ex: fibon(1); => 1
 Ex: fibon(0); => 1
 
-fib(n): 1 1 2 3 5 8 13 21
+fib(n): 1 1  23 5 8 13 21
         | | | | | |  |  |
 n:      0 1 2 3 4 5  6  7
 */
+function fibon(x) {
+  if (x === 1) {
+    return 1;
+  }
+
+  return x + fibon(x - 1);
+
+}
+// console.log(fibon(5));
+
+
+function fibonacci(num) {
+  if (num <= 1) {
+    return 1
+  };
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 
 /*
+5
+Create a function called sum2
+that take two parameter
+and will return the sumation from the first number to the second number
+Ex: sum2(4, 5); => 4 + 5 => 9
+Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18 
+*/
+
+function sum2(x, z) {
+  if (x === z) {
+    return z;
+
+  }
+  return x + sum2(x + 1, z)
+
+
+}
+// console.log(sum2(3, 6));
+
+/*
+6
+Create a function called repeatStr2
+that takes 2 string parameters,
+and return the first string number time
+(of the lenght of the secend string) with space betweeen it
+Important: the stop condition should be [somthing=== 0]
+Ex: repeatStr2("ro","cc"); => "ro ro"
+Ex: repeatStr2("ro","fff"); => "ro ro ro"
+*/
+
+function repeatStr2(str1, str2) {
+  if (str2.length === 0) {
+    return " ";
+  }
+  return str1 + repeatStr2(str1, str2.substr(1));
+
+
+}
+// console.log(repeatStr2("a", "1111"))
 
 
 
 
+/*
+7
+Create a function called multiOf
+that takes 3 parameters,
+and return the first number mutiple by
+(the second one ^ the third number)
+** Important: dont use **
+Ex: multiOf(4,10,3); => 4000
+Ex: multiOf(6,3,2); => 54
+Ex: multiOf(6,2,3); => 48
+*/
+function multiOf(num1, num2, num3) {
+  if (num1 === 0) {
+    return 0;
+
+  }
+
+  return num1 + multiOf(num1 -1);
+
+}
+console.log(multiOf(4,10,3));
+/*
+8
+Create a function called muti2
+that take two parameter
+and will return the multiplication from the first number to the second number
+Ex: muti2(4, 5); => 4 * 5 => 20
+Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
 */
 
 
 /*
+9
+Create a function called numberBetweenUs
+that take 2 parameters
+and return the number between them
+** Important: You should dint have extra space at the end or comma
+** Important: the stop condition should [num1===num2-1]
+numberBetweenUs(2,8) => "3, 4, 5, 6, 7"
+numberBetweenUs(1,3) => "2"
+*/
+function numberBetweenUs(num1, num2) {
 
+}
 
+/*
+10
+Write a function called countDown
+that takes one parameter
+and return a string represent the count down from the number
+that insert to 0
+instead of 0 need to ahve "done"
+between each number a comma and space
+** you need to use recursion
+** Important: You should dint have extra space at the end or comma
+countDown(5)
+=> "5, 4, 3, 2, 1, done"
+countDown(2)
+=> "2, 1, done"
+countDown(7)
+=> "7, 6, 5, 4, 3, 2, 1, done"
+*/
 
+/*
+11
+Write a function called multiplication2
+that takes two parameters
+and return the multiplication of them
+you need to use only sum
+multiplication2(5,4) => 20
+multiplication2(2,8) => 16
+multiplication2(7,6) =>  42
+*/
 
+/*
+11
+Write a function called mod2
+that takes two parameters
+and return the module of them
+without using %
+mod2(5,4) => 1
+mod2(2,8) => 2
+mod2(7,4) => 3
 */
 
 
 /*
-
-
-
-
+12
+Write a function called mirror
+that takes one parameter a string
+and return the reversed string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+** Important: You should dint have extra space at the end
+mirror("school") => "loohcs"
+mirror("car") => "rac"
+mirror("maDrasa") => "asarDam"
 */
-
 
 /*
-
-
-
-
+13
+Write a function called mirrorCaseAlso
+that takes one parameter a string
+and return the reversed string order
+and the lower case to upper case
+and the upper case to lower case
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+** Important: You should dint have extra space at the end
+mirrorCaseAlso("sChOol") => "LOoHcS"
+mirrorCaseAlso("THOR") => "roht"
+mirrorCaseAlso("BaBa") => "AbAb"
 */
-
 
 /*
-
-
-
-
+14
+Write a function called repeatChar
+that takes two parameters
+and return the times that this char
+without case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+repeatChar("schOol","o") => 2
+repeatChar("school","a") => 0
+repeatChar("School","s") => 1
+try more case by yourself
 */
-
 
 /*
-
-
-
-
+15
+Write a function called repeatChar2
+that takes two parameters
+and return the times that this char
+wit case sensetive
+repeat inside this string
+**hint: "str"[0] => "s"
+**hint: "car"[1] => "a"
+**hint: seacrh in MDN how "a" => "A" and "A" => "a"
+repeatChar2("schOol","o") => 1
+repeatChar2("school","a") => 0
+repeatChar2("School","S") => 1
+try more case by yourself
 */
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
-/*
-
-
-
-
-*/
-
-
